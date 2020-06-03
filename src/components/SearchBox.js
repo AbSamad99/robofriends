a276caf658f5a02891865c3e19d6cd81c4da2e8b
AbSamad99 <abdussamad.mi5@gmail.com>
2020-06-03 11:43:07 +0530

    minor edits #5

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,6 +3,7 @@ import React from 'react';
 const SearchBox = ({ searchChange }) => {
   return (
     <input
+      aria-label="Search Robots"
       className="pa2 ba b--green bg-lightest-blue ma2"
       type="search"
       placeholder="search robots"
